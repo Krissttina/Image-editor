c1ddc940af9fe012d73c0c4a9549d21bd6d236fa
Krissttina <krismetodieva123@gmail.com>
2022-08-04 17:54:17 +0300

    saving prog js part problem occured

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
-const fileInput = document.querySelector(".file-input"),
-chooseImgBtn = document.querySelector(".choose-img");
+const fileInput = document.querySelector(".file-input");
+const filterOptions = document.querySelectorAll(".filter button");
+const filterName = document.querySelector(".filter-info .name");
+const filterSlider = document.querySelector(".slider input");
+const rotateOptions = document.querySelectorAll(".rotate button");
+const filterValue = document.querySelector(".filter-info .value");
+const previewImg = document.querySelector(".preview-img img");
+const chooseImgBtn = document.querySelector(".choose-img");
 
-chooseImgBtn.addEventListener("click", () => fileInput.click());
+const loadImage = () => {
+    let file = fileInput.files[0]; //getting user selected file
+    if(!file) return; //return if user hasn't selected file
+    previewImg.src = URL.createObjectURL(file); // passing file url as preview img src
+    previewImg.addEventListener("load", () => {
+        document.querySelector(".conteiner").classList.remove("disable");
+    });
+}
+
+filterOptions.forEach(option => {
+    option.addEventListener("click", () => { //adding click event listener to all filter btns
+        document.querySelector(".filter . active").classList.remove("active");
+        option.classList.add("active");
+        filterName.innerText = option.innerText;
+    })
+});
+
+const updateFilter = () => {
+    filterValue.innerText = `${filterSlider.value}%`;
+}
+
+fileInput.addEventListener('change', loadImage);
+filterSlider.addEventListener("input", updateFilter);
+chooseImgBtn.addEventListener("click", () => fileInput.click()); //loading img folder
 
 /*
-filterOptions = document.querySelectorAll(".filter button"),
-filterName = document.querySelector(".filter-info .name"),
-filterValue = document.querySelector(".filter-info .value"),
-filterSlider = document.querySelector(".slider input"),
-rotateOptions = document.querySelectorAll(".rotate button"),
-previewImg = document.querySelector(".preview-img img"),
+
 resetFilterBtn = document.querySelector(".reset-filter"),
 
 saveImgBtn = document.querySelector(".save-img");
 let brightness = "100", saturation = "100", inversion = "0", grayscale = "0";
 let rotate = 0, flipHorizontal = 1, flipVertical = 1;
-const loadImage = () => {
-    let file = fileInput.files[0];
-    if(!file) return;
-    previewImg.src = URL.createObjectURL(file);
-    previewImg.add
-}*/
+*/
